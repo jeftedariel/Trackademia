e180d6b60ebb2738c090a92fbac02dc1aace8d2f
jeftedariel <jeftedariel3@gmail.com>
2024-10-06 22:16:58 -0600

    init

---
+++ src/main/java/edu/utn/trackademia/Trackademia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.utn.trackademia;
+
+/**
+ *
+ * @author jefte
+ */
+public class Trackademia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
