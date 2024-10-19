d28558ec6dafd48c6d1ccdf6f71a2bc86abbab63
jeftedariel <jeftedariel3@gmail.com>
2024-10-19 17:57:40 -0600

    Menu for user managemnt

--- src/main/java/edu/utn/trackademia/controller/UserManagementController.java
+++ src/main/java/edu/utn/trackademia/controller/UserManagementController.java
@@ -4,21 +4,25 @@
  */
 package edu.utn.trackademia.controller;
 
-import edu.utn.trackademia.dao.RoleDAO;
 import edu.utn.trackademia.entities.UserSession;
+import io.github.palexdev.materialfx.controls.MFXTableColumn;
+import io.github.palexdev.materialfx.controls.MFXTableView;
+import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -27,47 +31,27 @@ import javafx.stage.StageStyle;
  *
  * @author jefte
  */
-public class MenuController implements Initializable {
+public class UserManagementController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    RoleDAO rdao = new RoleDAO(); 
-    
-    
     @FXML
     private ImageView logout;
-    
+
     @FXML
     private Label username;
 
     @FXML
-    private TitledPane educational;
-    @FXML
-    private TitledPane administration;
-    
-    @FXML
-    private Pane userManagement;
-    
+    private MFXTableView table;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        List<String> users = new ArrayList<String>();
+
         logout.setOnMouseClicked(event -> {
             logout();
         });
-        
+
         this.username.setText(UserSession.getInstance().getUserFullName());
-        
-        userManagement.setOnMouseClicked(event -> {
-            userManagement();
-        }); 
-        
-        educational.setCollapsible(false);
-        administration.setCollapsible(false);
-        
-        //Sets visible both panels (Administration & Educational) if permissions 'Show Educational' or 'Show Administration' are present
-        educational.setVisible(rdao.getPermissions(UserSession.getInstance().getRole()).stream().anyMatch(n->n.name().equals("Show Educational")));
-        administration.setVisible(rdao.getPermissions(UserSession.getInstance().getRole()).stream().anyMatch(n->n.name().equals("Show Administration")));
-        
+
     }
 
     public void logout() {
@@ -90,18 +74,4 @@ public class MenuController implements Initializable {
         spStage.close();
     }
 
-    public void userManagement() {
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(getClass().getResource("/fxml/UserManagement.fxml"));
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
-        Scene scene = new Scene(root);
-        Stage stage = (Stage) logout.getScene().getWindow();
-        stage.setScene(scene);
-    }
-    
-    
 }
-
